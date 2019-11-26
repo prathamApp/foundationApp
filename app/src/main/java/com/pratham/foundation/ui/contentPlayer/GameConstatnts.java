713c0a54f68415ff6475b2dffcb361b8427ab362
prathamApp <prathamApp>
2019-11-26 13:35:56 +0530

    Amar

--- app/src/main/java/com/pratham/foundation/ui/contentPlayer/GameConstatnts.java
+++ app/src/main/java/com/pratham/foundation/ui/contentPlayer/GameConstatnts.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -26,6 +27,8 @@ import com.pratham.foundation.ui.contentPlayer.listenAndWritting.ListeningAndWri
 import com.pratham.foundation.ui.contentPlayer.multipleChoiceQuetion.multipleChoiceFragment;
 import com.pratham.foundation.ui.contentPlayer.new_reading_fragment.ContentReadingFragment;
 import com.pratham.foundation.ui.contentPlayer.new_reading_fragment.ContentReadingFragment_;
+import com.pratham.foundation.ui.contentPlayer.new_vocab_reading.VocabReadingFragment;
+import com.pratham.foundation.ui.contentPlayer.new_vocab_reading.VocabReadingFragment_;
 import com.pratham.foundation.ui.contentPlayer.paragraph_writing.ParagraphWritingFragment_;
 import com.pratham.foundation.ui.contentPlayer.pictionary.pictionaryFragment;
 import com.pratham.foundation.ui.contentPlayer.reading.ReadingFragment;
@@ -45,7 +48,7 @@ public class GameConstatnts implements ShowInstruction {
     public static final String KEYWORD_MAPPING = "chKeywords";
     public static final String PARAGRAPH_WRITING = "CWiritng";
     public static final String LISTNING_AND_WRITTING = "Dict";
- //   public static final String READING = "reading";
+    //   public static final String READING = "reading";
 //    public static final String MULTIPLE_CHOICE_QUE = "multiple_choice_que";
     public static final String FILL_IN_THE_BLANKS = "fill_in_the_blanks";
     public static final String TRUE_FALSE = "true_false";
@@ -68,12 +71,13 @@ public class GameConstatnts implements ShowInstruction {
     public static final String VIDEO = "Video";
 
     public static boolean playInsequence = false;
-
-
+    public static ContentTable contentTable1;
+    public static InstructionsDialog instructionsDialog;
+    private static GameConstatnts gameConstatnts;
     public static List<ContentTable> gameList;
     public static int currentGameAdapterposition;
 
-    public static Bundle findGameData(String resourceID) {
+   /* public static Bundle findGameData(String resourceID) {
         for (int i = 0; i < gameList.size(); i++) {
             if (resourceID.equalsIgnoreCase(gameList.get(i).getResourceId())) {
                 if (i < (gameList.size() - 1)) {
@@ -90,7 +94,7 @@ public class GameConstatnts implements ShowInstruction {
             }
         }
         return null;
-    }
+    }*/
 
     /*public static Bundle isContains(String resourceID) {
         for (int i = 0; i < gameList.size(); i++) {
@@ -189,106 +193,160 @@ public class GameConstatnts implements ShowInstruction {
 
     public static void plaGame(Context context) {
 
-        ContentTable contentTable1 = null;
-        Bundle bundle = null;
+        contentTable1 = null;
 
         if (gameList != null && (currentGameAdapterposition < (gameList.size() - 1))) {
             //currentGameAdapterposition is initialized in adapter when game is played
             currentGameAdapterposition++;
             contentTable1 = gameList.get(currentGameAdapterposition);
-            bundle = new Bundle();
+            if (contentTable1 != null) {
+                playInsequence = true;
+                gameSelector(context, contentTable1);
+            }
+        } else {
+            ((ContentPlayerActivity) context).getSupportFragmentManager().popBackStack(SequenceLayout_.class.getSimpleName(), 0);
+        }
+    }
+
+    public static void gameSelector(Context context, ContentTable contentTable) {
+        contentTable1=contentTable;
+
+        instructionsDialog = new InstructionsDialog(getGameConstantInstance(),context, contentTable1.getResourceType());
+        instructionsDialog.show();
+
+        Bundle bundle = null;
+        bundle = new Bundle();
+        bundle.putString("contentPath", contentTable1.getResourcePath());
+        bundle.putString("StudentID", FC_Constants.currentStudentID);
+        bundle.putString("resId", contentTable1.getResourceId());
+        bundle.putString("contentName", contentTable1.getNodeTitle());
+        bundle.putString("sttLang", contentTable1.getContentLanguage());
+        bundle.putBoolean("onSdCard", true);
+        bundle.putString("jsonName", contentTable1.getResourceType());
+
+        switch (contentTable1.getResourceType()) {
+            case GameConstatnts.FACTRETRIEVAL:
+                FC_Utility.showFragment((Activity) context, new FactRetrieval_(), R.id.RL_CPA,
+                        bundle, FactRetrieval_.class.getSimpleName());
+                break;
+            case GameConstatnts.KEYWORD_IDENTIFICATION:
+                FC_Utility.showFragment((Activity) context, new KeywordsIdentificationFragment_(), R.id.RL_CPA,
+                        bundle, KeywordsIdentificationFragment_.class.getSimpleName());
+                break;
+            case GameConstatnts.KEYWORD_MAPPING:
+                FC_Utility.showFragment((Activity) context, new KeywordMappingFragment_(), R.id.RL_CPA,
+                        bundle, KeywordMappingFragment_.class.getSimpleName());
+                break;
+            case GameConstatnts.PARAGRAPH_WRITING:
+                if (FC_Constants.currentSubject.equalsIgnoreCase("English")) {
+                    FC_Utility.showFragment((Activity) context, new WordWritingFragment_(), R.id.RL_CPA,
+                            bundle, WordWritingFragment_.class.getSimpleName());
+                } else if (FC_Constants.currentSubject.equalsIgnoreCase("Science")) {
+                    FC_Utility.showFragment((Activity) context, new ParagraphWritingFragment_(), R.id.RL_CPA,
+                            bundle, ParagraphWritingFragment_.class.getSimpleName());
+                } else {
+                    if (FC_Constants.currentLevel <= 2) {
+                        FC_Utility.showFragment((Activity) context, new WordWritingFragment_(), R.id.RL_CPA,
+                                bundle, WordWritingFragment_.class.getSimpleName());
+                    } else {
+                        FC_Utility.showFragment((Activity) context, new ParagraphWritingFragment_(), R.id.RL_CPA,
+                                bundle, ParagraphWritingFragment_.class.getSimpleName());
+                    }
+                }
+                break;
+            case GameConstatnts.LISTNING_AND_WRITTING:
+                FC_Utility.showFragment((Activity) context, new ListeningAndWritting_(), R.id.RL_CPA,
+                        bundle, ListeningAndWritting_.class.getSimpleName());
+                break;
+            case GameConstatnts.MULTIPLE_CHOICE:
+                FC_Utility.showFragment((Activity) context, new multipleChoiceFragment(), R.id.RL_CPA,
+                        bundle, multipleChoiceFragment.class.getSimpleName());
+                break;
+
+            case GameConstatnts.READING_STT:
+                FC_Utility.showFragment((Activity) context, new ReadingFragment(), R.id.RL_CPA,
+                        bundle, ReadingFragment.class.getSimpleName());
+                break;
+
+            case "109":
+                FC_Utility.showFragment((Activity) context, new TrueFalseFragment(), R.id.RL_CPA,
+                        bundle, TrueFalseFragment.class.getSimpleName());
+                break;
+            case "110":
+                FC_Utility.showFragment((Activity) context, new FillInTheBlanksFragment(), R.id.RL_CPA,
+                        bundle, FillInTheBlanksFragment.class.getSimpleName());
+                break;
+            case GameConstatnts.SHOW_ME_ANDROID:
+                FC_Utility.showFragment((Activity) context, new pictionaryFragment(), R.id.RL_CPA,
+                        bundle, pictionaryFragment.class.getSimpleName());
+                break;
+
+
+            case GameConstatnts.THINKANDWRITE:
+            case GameConstatnts.DOING_ACT_READ:
+            case GameConstatnts.DOING_ACT_VIDEO:
+            case GameConstatnts.LetterWriting:
+                FC_Utility.showFragment((Activity) context, new DoingFragment(), R.id.RL_CPA,
+                        bundle, DoingFragment.class.getSimpleName());
+                break;
+            case GameConstatnts.VIDEO:
+                Intent intent = new Intent(context, ActivityVideoView_.class);
+                intent.putExtra("contentPath", contentTable1.getResourcePath());
+                intent.putExtra("StudentID", FC_Constants.currentStudentID);
+                intent.putExtra("resId", contentTable1.getResourceId());
+                intent.putExtra("contentName", contentTable1.getNodeTitle());
+                intent.putExtra("onSdCard", true);
+                context.startActivity(intent);
+//                FC_Utility.showFragment((Activity) context, new ActivityVideoView_(), R.id.RL_CPA,
+//                        bundle, ActivityVideoView.class.getSimpleName());
+                break;
+        }
+
+
+    }
+
+    @Override
+    public void play(Context context) {
+        if (contentTable1 != null) {
+            Bundle bundle = new Bundle();
             bundle.putString("contentPath", contentTable1.getResourcePath());
             bundle.putString("StudentID", FC_Constants.currentStudentID);
             bundle.putString("resId", contentTable1.getResourceId());
             bundle.putString("contentName", contentTable1.getNodeTitle());
+            bundle.putString("sttLang", contentTable1.getContentLanguage());
             bundle.putBoolean("onSdCard", true);
             bundle.putString("jsonName", contentTable1.getResourceType());
-            InstructionsDialog instructionsDialog = new InstructionsDialog(context, contentTable1.getResourceType());
-            instructionsDialog.show();
-            playInsequence = true;
-            if (contentTable1 != null)
-                switch (contentTable1.getResourceType()) {
-                    case GameConstatnts.FACTRETRIEVAL:
-                        FC_Utility.showFragment((Activity) context, new FactRetrieval_(), R.id.RL_CPA,
-                                bundle, FactRetrieval_.class.getSimpleName());
-                        break;
-                    case GameConstatnts.KEYWORD_IDENTIFICATION:
-                        FC_Utility.showFragment((Activity) context, new KeywordsIdentificationFragment_(), R.id.RL_CPA,
-                                bundle, KeywordsIdentificationFragment_.class.getSimpleName());
-                        break;
-                    case GameConstatnts.KEYWORD_MAPPING:
-                        FC_Utility.showFragment((Activity) context, new KeywordMappingFragment_(), R.id.RL_CPA,
-                                bundle, KeywordMappingFragment_.class.getSimpleName());
-                        break;
-                    case GameConstatnts.PARAGRAPH_WRITING:
-                        if (FC_Constants.currentSubject.equalsIgnoreCase("English")) {
-                            FC_Utility.showFragment((Activity) context, new WordWritingFragment_(), R.id.RL_CPA,
-                                    bundle, WordWritingFragment_.class.getSimpleName());
-                        } else if(FC_Constants.currentSubject.equalsIgnoreCase("Science")){
-                            FC_Utility.showFragment((Activity) context, new ParagraphWritingFragment_(), R.id.RL_CPA,
-                                    bundle, ParagraphWritingFragment_.class.getSimpleName());
-                        }else {
-                            if (FC_Constants.currentLevel <= 2) {
-                                FC_Utility.showFragment((Activity) context, new WordWritingFragment_(), R.id.RL_CPA,
-                                        bundle, WordWritingFragment_.class.getSimpleName());
-                            } else {
-                                FC_Utility.showFragment((Activity) context, new ParagraphWritingFragment_(), R.id.RL_CPA,
-                                        bundle, ParagraphWritingFragment_.class.getSimpleName());
-                            }
+            playInsequence = false;
+            final Handler handler = new Handler();
+            switch (contentTable1.getResourceType()) {
+                case GameConstatnts.READ_VOCAB_ANDROID:
+                    FC_Utility.showFragment((Activity) context, new VocabReadingFragment_(), R.id.RL_CPA,
+                            bundle, VocabReadingFragment.class.getSimpleName());
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            instructionsDialog.dismiss();
+                            //Do something after 100ms
                         }
+                    }, 100);
+                    break;
+                case GameConstatnts.PICTIONARYFRAGMENT:
+                    FC_Utility.showFragment((Activity) context, new ContentReadingFragment_(), R.id.RL_CPA,
+                            bundle, ContentReadingFragment.class.getSimpleName());
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            instructionsDialog.dismiss();
+                            //Do something after 100ms
+                        }
+                    }, 100);
+                    break;
+                    default:
+                        instructionsDialog.dismiss();
                         break;
-                    case GameConstatnts.LISTNING_AND_WRITTING:
-                        FC_Utility.showFragment((Activity) context, new ListeningAndWritting_(), R.id.RL_CPA,
-                                bundle, ListeningAndWritting_.class.getSimpleName());
-                        break;
-                    case GameConstatnts.MULTIPLE_CHOICE:
-                        FC_Utility.showFragment((Activity) context, new multipleChoiceFragment(), R.id.RL_CPA,
-                                bundle, multipleChoiceFragment.class.getSimpleName());
-                        break;
+            }
 
-                    case GameConstatnts.READING_STT:
-                        FC_Utility.showFragment((Activity) context, new ReadingFragment(), R.id.RL_CPA,
-                                bundle, ReadingFragment.class.getSimpleName());
-                        break;
 
-                    case "109":
-                        FC_Utility.showFragment((Activity) context, new TrueFalseFragment(), R.id.RL_CPA,
-                                bundle, TrueFalseFragment.class.getSimpleName());
-                        break;
-                    case "110":
-                        FC_Utility.showFragment((Activity) context, new FillInTheBlanksFragment(), R.id.RL_CPA,
-                                bundle, FillInTheBlanksFragment.class.getSimpleName());
-                        break;
-                    case GameConstatnts.SHOW_ME_ANDROID:
-                        FC_Utility.showFragment((Activity) context, new pictionaryFragment(), R.id.RL_CPA,
-                                bundle, pictionaryFragment.class.getSimpleName());
-                        break;
-
-                    case GameConstatnts.PICTIONARYFRAGMENT:
-                        FC_Utility.showFragment((Activity) context, new ContentReadingFragment_(), R.id.RL_CPA,
-                                bundle, ContentReadingFragment.class.getSimpleName());
-                        break;
-                    case GameConstatnts.THINKANDWRITE:
-                    case GameConstatnts.DOING_ACT_READ:
-                    case GameConstatnts.DOING_ACT_VIDEO:
-                    case GameConstatnts.LetterWriting:
-                        FC_Utility.showFragment((Activity) context, new DoingFragment(), R.id.RL_CPA,
-                                bundle, DoingFragment.class.getSimpleName());
-                        break;
-                    case GameConstatnts.VIDEO:
-                        Intent intent = new Intent(context, ActivityVideoView_.class);
-                        intent.putExtra("contentPath", contentTable1.getResourcePath());
-                        intent.putExtra("StudentID", FC_Constants.currentStudentID);
-                        intent.putExtra("resId", contentTable1.getResourceId());
-                        intent.putExtra("contentName", contentTable1.getNodeTitle());
-                        intent.putExtra("onSdCard", true);
-                        context.startActivity(intent);
-//                FC_Utility.showFragment((Activity) context, new ActivityVideoView_(), R.id.RL_CPA,
-//                        bundle, ActivityVideoView.class.getSimpleName());
-                        break;
-                }
-        } else {
-            ((ContentPlayerActivity) context).getSupportFragmentManager().popBackStack(SequenceLayout_.class.getSimpleName(), 0);
         }
     }
 
@@ -299,11 +357,11 @@ public class GameConstatnts implements ShowInstruction {
         return 1;
     }
 
-    public static void showInstructionDialog(ShowInstruction showInstruction, Activity context, String resorcetype) {
+/*    public static void showInstructionDialog(ShowInstruction showInstruction, Activity context, String resorcetype) {
         InstructionsDialog instructionsDialog = new InstructionsDialog(context, resorcetype);
         instructionsDialog.show();
 
-    }
+    }*/
 
     public static void showGameInfo(Context context, String info) {
         final Dialog dialog = new Dialog(context);
@@ -316,13 +374,15 @@ public class GameConstatnts implements ShowInstruction {
         dialog.show();
     }
 
-    @Override
-    public void play() {
-
-    }
 
     @Override
     public void exit() {
 
+    }
+    private static GameConstatnts getGameConstantInstance(){
+        if(gameConstatnts==null){
+            gameConstatnts=new GameConstatnts();
+        }
+        return gameConstatnts;
     }
 }
